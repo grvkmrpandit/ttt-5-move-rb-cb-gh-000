9499341a16b663383d46ddd13585b4ed869dc0fc
Gaurav Pandit <grvkmrpandit@gmail.com>
2019-08-06 03:41:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,5 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+x=gets.strip
+def input_to_index(board,x)
+  board[x]='X'
+
+  
 
 # code your input_to_index and move method here!
